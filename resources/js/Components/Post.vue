cf99abf659b30dd87b93f474363913ec23ff9a2b
Russell Jones <jonesrussell42@gmail.com>
2023-06-04 20:41:13 -0400

    Rename tweet to post

--- resources/js/Components/Post.vue
+++ resources/js/Components/Post.vue
@@ -8,7 +8,7 @@ import Sync from 'vue-material-design-icons/Sync.vue'
 import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue'
 import TrashCanOutline from 'vue-material-design-icons/TrashCanOutline.vue'
 
-defineProps({ tweet: Object });
+defineProps({ post: Object });
 
 let openOptions = ref(false);
 
@@ -16,13 +16,13 @@ let openOptions = ref(false);
 
 <template>
     <div class="min-w-[60px]">
-        <img class="rounded-full m-2 mt-3" width="50" :src="tweet.image">
+        <img class="rounded-full m-2 mt-3" width="50" :src="post.image">
     </div>
     <div class="p-2 w-full">
         <div class="font-extrabold flex items-center justify-between mt-0.5 mb-1.5">
             <div class="flex items-center">
-                <div>{{ tweet.name }}</div>
-                <span class="font-[300] text-[15px] text-gray-500 pl-2">{{ tweet.handle }}</span>
+                <div>{{ post.name }}</div>
+                <span class="font-[300] text-[15px] text-gray-500 pl-2">{{ post.handle }}</span>
             </div>
             <div class="hover:bg-gray-800 rounded-full cursor-pointer relative">
                 <button type="button" class="block p-2">
@@ -33,7 +33,7 @@ let openOptions = ref(false);
                         <Link
                             as="button"
                             method="delete"
-                            :href="route('tweets.destroy', { id: tweet.id })"
+                            :href="route('tweets.destroy', { id: post.id })"
                             class="flex items-center cursor-pointer"
                         >
                             <TrashCanOutline class="pr-3" fillColor="#DC2626" :size="18"/>
@@ -43,31 +43,31 @@ let openOptions = ref(false);
                 </div>
             </div>
         </div>
-        <div class="pb-3">{{ tweet.tweet }}</div>
-        <div v-if="tweet.file">
-            <div v-if="!tweet.is_video" class="rounded-xl">
-                <img :src="tweet.file" class="mt-2 object-fill rounded-xl w-full">
+        <div class="pb-3">{{ post.tweet }}</div>
+        <div v-if="post.file">
+            <div v-if="!post.is_video" class="rounded-xl">
+                <img :src="post.file" class="mt-2 object-fill rounded-xl w-full">
             </div>
             <div v-else>
-                <video class="rounded-xl" :src="tweet.file" controls></video>
+                <video class="rounded-xl" :src="post.file" controls></video>
             </div>
         </div>
         <div class="flex items-center justify-between mt-4 w-4/5">
             <div class="flex">
                 <MessageOutline fillColor="#5e5c5c" :size="18" />
-                <span class="text-xs font-extrabold text-[#5e5c5c] ml-3">{{ tweet.comments }}</span>
+                <span class="text-xs font-extrabold text-[#5e5c5c] ml-3">{{ post.comments }}</span>
             </div>
             <div class="flex">
                 <Sync fillColor="#5e5c5c" :size="18" />
-                <span class="text-xs font-extrabold text-[#5e5c5c] ml-3">{{ tweet.retweets }}</span>
+                <span class="text-xs font-extrabold text-[#5e5c5c] ml-3">{{ post.retweets }}</span>
             </div>
             <div class="flex">
                 <HeartOutline fillColor="#5e5c5c" :size="18" />
-                <span class="text-xs font-extrabold text-[#5e5c5c] ml-3">{{ tweet.likes }}</span>
+                <span class="text-xs font-extrabold text-[#5e5c5c] ml-3">{{ post.likes }}</span>
             </div>
             <div class="flex">
                 <ChartBar fillColor="#5e5c5c" :size="18" />
-                <span class="text-xs font-extrabold text-[#5e5c5c] ml-3">{{ tweet.analytics }}</span>
+                <span class="text-xs font-extrabold text-[#5e5c5c] ml-3">{{ post.analytics }}</span>
             </div>
 
         </div>
--- resources/js/Pages/Explore.vue
+++ resources/js/Pages/Explore.vue
@@ -16,7 +16,7 @@
         <div class="text-white">
 
             <div class="flex" v-for="tweet in tweets" :key="tweet">
-                <Post :tweet="tweet"/>
+                <Post :post="tweet"/>
             </div>
             <div class="border-b border-b-gray-800 mt-2"></div>
         </div>
