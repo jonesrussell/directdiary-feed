513cb7d08c006cdc9c7c16d8ee1350520e79a628
Russell Jones <jonesrussell42@gmail.com>
2023-06-04 20:29:58 -0400

    Hide For You / Following from anonymous users

--- resources/js/Layouts/TwitterLayout.vue
+++ resources/js/Layouts/TwitterLayout.vue
@@ -87,12 +87,12 @@ const textareaInput = (e) => {
             </section>
 
             <section class="lg:w-7/12 w-11/12 border-x border-gray-800 relative">
-                <div class=" bg-black bg-opacity-50 backdrop-blur-md z-10 absolute w-full">
+                <div class="bg-black bg-opacity-50 backdrop-blur-md z-10 absolute w-full">
                     <div class="border-gray-800 border-b w-full">
                         <div class="w-full text-white text-[22px] font-extrabold p-4">
                             Explore
                         </div>
-                        <div class="flex">
+                        <div class="flex" v-if="authUser">
                             <div
                                 class="flex items-center justify-center w-full h-[60px] text-white text-[17px] font-extrabold p-4 hover:bg-gray-500 hover:bg-opacity-30 cursor-pointer transition duration-200 ease-in-out">
                                 <div class="inline-block text-center border-b-4 border-b-[#1C9CEF] h-[60px]">
@@ -108,7 +108,8 @@ const textareaInput = (e) => {
                 </div>
 
                 <div class="absolute top-0 z-0 h-full w-full overflow-auto scrollbar-hide">
-                    <div class="mt-[126px]"></div>
+                    <div class="mt-[80px]" v-if="!authUser"></div>
+                    <div class="mt-[126px]" v-if="authUser"></div>
                     <slot />
                     <div class="pb-4"></div>
                 </div>
