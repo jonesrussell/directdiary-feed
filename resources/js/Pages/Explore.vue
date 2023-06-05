383b4b1d530c6e74fdb44472df20a552e1af3d5f
Russell Jones <jonesrussell42@gmail.com>
2023-06-04 20:33:01 -0400

    Rename Tweet component to Food

--- resources/js/Pages/Explore.vue
+++ resources/js/Pages/Explore.vue
@@ -1,7 +1,7 @@
 <script setup>
     import { ref } from 'vue';
     import { Head, Link } from '@inertiajs/vue3';
-    import Tweet from '@/Components/Tweet.vue'
+    import Food from '@/Components/Food.vue'
     import TwitterLayout from '@/Layouts/TwitterLayout.vue';
 
     defineProps({ tweets: Array })
@@ -16,7 +16,7 @@
         <div class="text-white">
 
             <div class="flex" v-for="tweet in tweets" :key="tweet">
-                <Tweet :tweet="tweet"/>
+                <Food :tweet="tweet"/>
             </div>
             <div class="border-b border-b-gray-800 mt-2"></div>
         </div>
