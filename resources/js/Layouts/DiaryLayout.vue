9d10fcdc45b4125f8e2156ab8b685d8008cc56c7
Russell Jones <jonesrussell42@gmail.com>
2023-06-09 19:51:25 -0400

    Public profile WIP

--- resources/js/Layouts/DiaryLayout.vue
+++ resources/js/Layouts/DiaryLayout.vue
@@ -16,6 +16,7 @@ import MenuItem from '@/Components/MenuItem.vue'
 const props = defineProps({
     title: String,
     showForYouFollowing: Boolean,
+    showProfileTabs: Boolean,
 });
 
 const page = usePage();
@@ -34,7 +35,7 @@ let randImg2 = ref(`https://picsum.photos/id/${(Math.random() * 200).toFixed(0)}
 
 const getFile = (e) => {
     file.value = e.target.files[0]
-	showUpload.value = URL.createObjectURL(e.target.files[0]);
+    showUpload.value = URL.createObjectURL(e.target.files[0]);
     uploadType.value = file.value.name.split('.').pop();
 }
 
@@ -66,7 +67,7 @@ const textarea = ref(null);
 const textareaInput = (e) => {
     textarea.value.style.height = "auto";
     textarea.value.style.height = `${e.target.scrollHeight}px`;
-  };
+};
 
 </script>
 
@@ -79,28 +80,27 @@ const textareaInput = (e) => {
                 </div>
 
                 <Link href="/" v-if="authUser">
-                    <MenuItem iconString="Home"/>
+                <MenuItem iconString="Home" />
                 </Link>
                 <Link href="/explore">
-                    <MenuItem iconString="Explore"/>
+                <MenuItem iconString="Explore" />
                 </Link>
 
-                <MenuItem iconString="Notifications" v-if="authUser"/>
+                <MenuItem iconString="Notifications" v-if="authUser" />
                 <Link href="/messages">
-                    <MenuItem iconString="Messages" v-if="authUser"/>
-                </Link>
-                
-                <Link href="/profile">
-                    <MenuItem iconString="Profile" v-if="authUser"/>
+                <MenuItem iconString="Messages" v-if="authUser" />
                 </Link>
 
-                <button
-                    v-if="authUser"
-                    @click="createPost = true"
-                    class="lg:w-full mt-8 ml-2 text-white font-extrabold text-[22px] bg-[#1C9CEF] p-3 px-3 rounded-full cursor-pointer"
-                >
+                <Link href="/profile">
+                <MenuItem iconString="Profile" v-if="authUser" />
+                </Link>
+
+                <button v-if="authUser" @click="createPost = true"
+                    class="lg:w-full mt-8 ml-2 text-white font-extrabold text-[22px] bg-[#1C9CEF] p-3 px-3 rounded-full cursor-pointer">
                     <span class="lg:block hidden">Post</span>
-                    <span class="block lg:hidden"><Feather /></span>
+                    <span class="block lg:hidden">
+                        <Feather />
+                    </span>
                 </button>
             </section>
 
@@ -110,6 +110,7 @@ const textareaInput = (e) => {
                         <div class="w-full text-white text-[22px] font-extrabold p-4">
                             {{ title }}
                         </div>
+
                         <div class="flex" v-if="showForYouFollowing">
                             <div
                                 class="flex items-center justify-center w-full h-[60px] text-white text-[17px] font-extrabold p-4 hover:bg-gray-500 hover:bg-opacity-30 cursor-pointer transition duration-200 ease-in-out">
@@ -136,9 +137,8 @@ const textareaInput = (e) => {
 
             <section class="lg:block hidden lg:w-4/12 h-screen border-l border-gray-800 pl-4">
                 <div class="w-full p-1 mt-2 px-4 lg:flex items-center rounded-full hidden bg-[#212327]">
-                <Magnify fillColor="#5e5c5c" :size="25"/>
-                    <input
-                        class="
+                    <Magnify fillColor="#5e5c5c" :size="25" />
+                    <input class="
                             appearance-none
                             w-full
                             border-0
@@ -148,10 +148,7 @@ const textareaInput = (e) => {
                             placeholder-gray-500
                             leading-tight
                             focus:ring-0
-                        "
-                        type="text"
-                        placeholder="Search Diary"
-                    >
+                        " type="text" placeholder="Search Diary">
                 </div>
 
                 <div class="w-full mt-4 rounded-lg lg:block hidden bg-[#212327]">
@@ -163,7 +160,7 @@ const textareaInput = (e) => {
                                 <div class="text-[14px] text-gray-400">Tennis Tournament LIVE</div>
                                 <div class="w-full text-white font-extrabold mb-6 text-[17px]">Australian Open 2023</div>
                             </div>
-                            <img class="rounded-xl"  :src="randImg2" alt="" />
+                            <img class="rounded-xl" :src="randImg2" alt="" />
                         </div>
                     </div>
 
@@ -174,7 +171,7 @@ const textareaInput = (e) => {
                                 <div class="w-full text-white font-extrabold text-[17px]">Doomsday Clock</div>
                                 <div class="text-[14px] text-gray-400">5,923 Posts</div>
                             </div>
-                            <DotsHorizontal fillColor="#5e5c5c"/>
+                            <DotsHorizontal fillColor="#5e5c5c" />
                         </div>
                     </div>
 
@@ -185,7 +182,7 @@ const textareaInput = (e) => {
                                 <div class="w-full text-white font-extrabold text-[17px]">Poro</div>
                                 <div class="text-[14px] text-gray-400">12,432 Posts</div>
                             </div>
-                            <DotsHorizontal fillColor="#5e5c5c"/>
+                            <DotsHorizontal fillColor="#5e5c5c" />
                         </div>
                     </div>
 
@@ -196,7 +193,7 @@ const textareaInput = (e) => {
                                 <div class="w-full text-white font-extrabold text-[17px]">Sunak</div>
                                 <div class="text-[14px] text-gray-400">98,745 Posts</div>
                             </div>
-                            <DotsHorizontal fillColor="#5e5c5c"/>
+                            <DotsHorizontal fillColor="#5e5c5c" />
                         </div>
                     </div>
 
@@ -206,63 +203,48 @@ const textareaInput = (e) => {
                 </div>
             </section>
         </div>
-        <footer
-            v-if="!authUser"
-            class="absolute text-white bottom-0 w-full bg-red-400 h-20 flex space-x-4 justify-center items-center"
-        >
-            <Link
-                    href="/login"
-                    class="px-4 py-2 border text-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Log in
+        <footer v-if="!authUser"
+            class="absolute text-white bottom-0 w-full bg-red-400 h-20 flex space-x-4 justify-center items-center">
+            <Link href="/login"
+                class="px-4 py-2 border text-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Log in
             </Link>
-            <Link
-                    href="/register"
-                    class="px-4 py-2 border text-gray-800 bg-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Sign up
+            <Link href="/register"
+                class="px-4 py-2 border text-gray-800 bg-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Sign up
             </Link>
         </footer>
     </div>
 
-    <div id="OverlaySection" v-if="createPost" class="fixed top-0 left-0 w-full h-screen bg-black md:bg-gray-400 md:bg-opacity-30 md:p-3">
+    <div id="OverlaySection" v-if="createPost"
+        class="fixed top-0 left-0 w-full h-screen bg-black md:bg-gray-400 md:bg-opacity-30 md:p-3">
         <div class="md:max-w-2xl md:mx-auto md:mt-10 md:rounded-xl bg-black">
 
             <div class=" flex items-center justify-between md:inline-block p-2 m-2 rounded-full cursor-pointer">
                 <div @click="closeMessageBox()" class="hover:bg-gray-800 inline-block p-2 rounded-full cursor-pointer">
-                    <Close fillColor="#FFFFFF" :size="28" class="md:block hidden"/>
-                    <ArrowLeft fillColor="#FFFFFF" :size="28" class="md:hidden block"/>
+                    <Close fillColor="#FFFFFF" :size="28" class="md:block hidden" />
+                    <ArrowLeft fillColor="#FFFFFF" :size="28" class="md:hidden block" />
                 </div>
 
-                <button
-                    :class="post ? 'bg-[#1C9CEF] text-white' : 'bg-[#124D77] text-gray-400'"
-                    :disabled="!post"
-                    @click="addPost()"
-                    class="md:hidden font-extrabold text-[16px] p-1.5 px-4 rounded-full cursor-pointer"
-                >
+                <button :class="post ? 'bg-[#1C9CEF] text-white' : 'bg-[#124D77] text-gray-400'" :disabled="!post"
+                    @click="addPost()" class="md:hidden font-extrabold text-[16px] p-1.5 px-4 rounded-full cursor-pointer">
                     Post
                 </button>
             </div>
             <div class="w-full flex">
                 <div class="ml-3.5 mr-2">
-                    <img class="rounded-full" width="55"  :src="randImg1" />
+                    <img class="rounded-full" width="55" :src="randImg1" />
                 </div>
                 <div class="w-[calc(100%-100px)]">
                     <div class="inline-block">
                         <div class="flex items-center border border-gray-700 rounded-full">
                             <span class="text-[#1C9CEF] p-0.5 pl-3.5 font-extrabold">Everyone</span>
-                            <ChevronDown class="pr-2" fillColor="#1C9CEF" :size="25"/>
+                            <ChevronDown class="pr-2" fillColor="#1C9CEF" :size="25" />
                         </div>
                     </div>
                     <div>
-                        <textarea
-                            ref="textarea"
-                            :oninput="textareaInput"
-                            v-model="post"
-                            placeholder="What's happening?"
-                            cols="30"
-                            rows="4"
-                            class="
+                        <textarea ref="textarea" :oninput="textareaInput" v-model="post" placeholder="What's happening?"
+                            cols="30" rows="4" class="
                                 w-full
                                 bg-black
                                 border-0
@@ -272,11 +254,10 @@ const textareaInput = (e) => {
                                 text-[19px]
                                 font-extrabold
                                 min-h-[120px]
-                            "
-                        ></textarea>
+                            "></textarea>
                     </div>
                     <div class="w-full">
-                        <video controls v-if="uploadType === 'mp4'" :src="showUpload" class="rounded-xl overflow-auto"/>
+                        <video controls v-if="uploadType === 'mp4'" :src="showUpload" class="rounded-xl overflow-auto" />
                         <img v-else :src="showUpload" class="rounded-xl min-w-full">
                     </div>
                     <div class="flex py-2 items-center text-[#1C9CEF] font-extrabold">
@@ -298,12 +279,9 @@ const textareaInput = (e) => {
                                 <Emoticon fillColor="#1C9CEF" :size="25" />
                             </div>
                         </div>
-                        <button
-                            :class="post ? 'bg-[#1C9CEF] text-white' : 'bg-[#124D77] text-gray-400'"
-                            :disabled="!post"
+                        <button :class="post ? 'bg-[#1C9CEF] text-white' : 'bg-[#124D77] text-gray-400'" :disabled="!post"
                             @click="addPost()"
-                            class=" hidden md:block font-extrabold text-[16px] p-1.5 px-4 rounded-full cursor-pointer"
-                        >
+                            class=" hidden md:block font-extrabold text-[16px] p-1.5 px-4 rounded-full cursor-pointer">
                             Post
                         </button>
                     </div>
--- resources/js/Pages/PublicProfile.vue
+++ resources/js/Pages/PublicProfile.vue
@@ -1,23 +1,36 @@
 <script setup>
-    import { ref } from 'vue';
-    import { Head, Link } from '@inertiajs/vue3';
-    import DiaryLayout from '@/Layouts/DiaryLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import DiaryLayout from '@/Layouts/DiaryLayout.vue';
 
 const props = defineProps({ profile: Array });
 
 console.log(props.profile);
+
 let username = props.profile.username;
 let fullname = `${props.profile.firstname} ${props.profile.lastname}`
 </script>
 
 <template>
-
     <Head :title="fullname" />
 
-    <DiaryLayout :title="username">
-        <div class="text-white">
-            {{ fullname }} @{{ username }}
-            <div class="border-b border-b-gray-800 mt-2"></div>
+    <DiaryLayout :title="fullname" showProfileTabs>
+        <div class="m-4 text-white">
+            <img :src="profile.avatar" class="rounded-full mb-4" />
+            <div>{{ fullname }}</div>
+            <div>@{{ username }}</div>
+        </div>
+
+        <div class="flex">
+            <div
+                class="flex items-center justify-center w-full h-[60px] text-white text-[17px] font-extrabold p-4 hover:bg-gray-500 hover:bg-opacity-30 cursor-pointer transition duration-200 ease-in-out">
+                <div class="inline-block text-center border-b-4 border-b-[#1C9CEF] h-[60px]">
+                    <div class="my-auto mt-4">Posts</div>
+                </div>
+            </div>
+            <div
+                class="w-full h-[60px] text-gray-500 text-[17px] font-extrabold p-4 hover:bg-gray-500 hover:bg-opacity-30 cursor-pointer transition duration-200 ease-in-out">
+                <div class="text-center">Domains</div>
+            </div>
         </div>
 
     </DiaryLayout>
