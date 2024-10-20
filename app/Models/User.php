04f62a82a2f1cc71363f1cb2cfe33566edd36e58
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-10-20 04:16:59 -0400

    Removed hashed option

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable implements HasMedia
 
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        // 'password' => 'hashed',
     ];
 
     protected function fullName(): Attribute
