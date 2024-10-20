8abda1e75b44c56d9361990bbb1cc0f3347c91ad
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-10-20 04:08:39 -0400

    unused domain edit

--- routes/web.php
+++ routes/web.php
@@ -69,8 +69,6 @@ Route::middleware('auth')->group(function () {
     Route::get('domains/create', [DomainController::class, 'create'])->name('domains.create');
     Route::post('domains', [DomainController::class, 'store'])->name('domains.store');
     Route::get('domains/{domain}', [DomainController::class, 'show'])->name('domains.show');
-    Route::get('domains/{domain}/edit', [DomainController::class, 'edit'])->name('domains.edit');
-    Route::put('domains/{domain}', [DomainController::class, 'update'])->name('domains.update');
     Route::delete('domains/{domain}', [DomainController::class, 'destroy'])->name('domains.destroy');
 });
 
